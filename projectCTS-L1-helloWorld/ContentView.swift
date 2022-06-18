99f26afe94ea6a06c34395816ca3fbf2e9d1e861
natruz <88256962+natruz@users.noreply.github.com>
2022-06-19 07:49:13 +0800

    Initial Commit

---
+++ projectCTS-L1-helloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  projectCTS-L1-helloWorld
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 19/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ projectCTS-L1-helloWorld/projectCTS_L1_helloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectCTS_L1_helloWorldApp.swift
+//  projectCTS-L1-helloWorld
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 19/6/22.
+//
+
+import SwiftUI
+
+@main
+struct projectCTS_L1_helloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
