95271aefc2682c440b141ffc5f6b8747de458338
natruz <88256962+natruz@users.noreply.github.com>
2022-06-19 09:27:31 +0800

    @State vars for the colours and updating the button to change the foreground and background colour of the label

--- projectCTS-L1-helloWorld/ContentView.swift
+++ projectCTS-L1-helloWorld/ContentView.swift
@@ -11,12 +11,29 @@ struct ContentView: View {
     
     @State var message = ["Hello World", "App Development", "SST ICT Dept"]
     @State var messageIndex = 0
+    @State var foregroundColour = Color.white
+    @State var backgroundColour = Color.red
     
     var body: some View {
         VStack {
             Text(message[messageIndex % message.count])
+                .font(.system(size: 36))
+                .frame(width: 374, height: 60)
+                .foregroundColor(foregroundColour)
+                .background(backgroundColour)
             Button {
                 messageIndex += 1
+                if message[messageIndex % message.count] == "App Development" {
+                    foregroundColour = Color.black
+                    backgroundColour = Color.blue
+                } else {
+                    foregroundColour = Color.white
+                    if message[messageIndex % message.count] == "SST ICT Dept" {
+                        backgroundColour = Color.green
+                    } else {
+                        backgroundColour = Color.red
+                    }
+                }
             } label: {
                 Text("Change Text")
             }
